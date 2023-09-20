670ccf310a71d550a7dd3cb6cd73430e21d6040e
caleb <cbrad022@odu.edu>
2023-09-20 13:56:25 -0400

    add favicon

--- assignments/Bradford/2/server.js
+++ assignments/Bradford/2/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const favicon = require('serve-favicon')
 const {createReadStream} = require('fs');
 const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(favicon(__dirname + '/favicon.ico'));
 
 app.get('/', (req, res) => {
     createReadStream('index.html').pipe(res)
@@ -20,7 +22,6 @@ app.get('/bear', (req, res) => {
 app.get('/everything', (req, res) => {
     createReadStream('everything.html').pipe(res)
 });
-
 
 app.listen(4000);
 console.log("Listening on port 4000");
