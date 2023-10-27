7192407a57580e99d84f956527680f98e4b7abe4
Greg <gregory.arcand.17@cnu.edu>
2023-10-27 15:29:12 -0400

    Added files from directory C:/Users/17572/cs533-f23/assignments/Arcand/4

---
+++ assignments/Arcand/4/server.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const htmlFilesDirectory = path.join(__dirname, 'framable');
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(htmlFilesDirectory, req.url);
+
+    if (!path.extname(filePath)) {
+        filePath = filePath + '.html';
+    }
+
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end('File Not Found');
+        } else {
+            const fileStream = fs.createReadStream(filePath);
+            fileStream.pipe(res);
+        }
+    });
+});
+
+server.listen(8080, () => {
+    console.log('Server is running on http://localhost:8080/');
+});
