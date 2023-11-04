57d73b5905d6617e4a03d28440ed4764bfb05d65
mallywal <mmage004@odu.edu>
2023-11-03 20:19:53 -0400

    program complete

--- assignments/magee/3/A3-main.py
+++ assignments/magee/3/A3-main.py
@@ -1,5 +1,11 @@
-import os, subprocess
+import os, subprocess, re, statistics
+from collections import OrderedDict
+from operator import itemgetter
 
+
+def sort_dict_by_value_lambda(test_dict):
+    sorted_list = sorted(test_dict.items(), key=lambda x: x[1])
+    return dict(sorted_list)
 
 file1 = 'domain_list.txt'
 file2 = 'problem_domains.txt'
@@ -25,20 +31,40 @@ with open(file1) as infile, open('data_out.txt', 'w') as outfile:
 
         count += 1
 
+print('\n' + '########################## Cookie Count ##########################' + '\n')
+with open('data_out.txt') as infile:
 
-with open('data_out.txt', 'r') as outfile:
-    # Cookie lists
-    http_only = []
-    secure = []
-    strict_same_site = []
-    lax_same_site = []
-    none_same_site = []
+    attribute_patterns = {
+        'HttpOnly': re.compile(r'HttpOnly', re.I),
+        'Secure': re.compile(r'Secure', re.I),
+        'SameSite': re.compile(r'SameSite', re.I),
+        'Path': re.compile(r'Path', re.I),
+        'None': re.compile(r'None', re.I)
+    }
+    attribute_counts = {
+        'HttpOnly': 0,
+        'Secure': 0,
+        'SameSite': 0,
+        'SameSite Strict': 0,
+        'SameSite Lax': 0,
+        'Path': 0,
+        'None': 0
+    }
 
-    for line in outfile:
-        if ''
 
+    for line in infile:
+        for attribute, pattern in attribute_patterns.items():
+            if pattern.search(line):
+                attribute_counts[attribute] += 1
 
+    c_list = []
+    for attribute, count in attribute_counts.items():
+        c_list.append(count)
+        print(f'{attribute}: {count}')
 
-#curl_table = []
-#curl_table.append(domain)
-#curl_table.append()
+print('\n' + '########################## Min/Max/Mean/Median for Cookie Count ##########################' + '\n')
+
+print(f'Min: {min(attribute_counts.values())}' )
+print(f'Max: {max(attribute_counts.values())}')
+print(f'Mean: {statistics.mean(list(attribute_counts.values()))}')
+print(f'Median: {statistics.mean(list(attribute_counts.values()))}')
