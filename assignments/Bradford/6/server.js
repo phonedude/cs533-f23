70bc50057e170c1521fba3520a89d91e6d399f23
caleb <cbrad022@odu.edu>
2023-11-26 20:25:39 -0500

    add logic to notify user how many times they have visited

--- assignments/Bradford/6/server.js
+++ assignments/Bradford/6/server.js
@@ -13,11 +13,11 @@ app.get('/', (req, res) => {
     const acceptHeader = req.headers['accept'];
     const acceptLanguage = req.headers['accept-language'];
     const userHash = md5(userAgent + acceptHeader + acceptLanguage);
-
     // Fingerprint exists; user has visited before
     if (fingerPrintLog[userHash]) {
         console.log('This user has visited before!');
         fingerPrintLog[userHash]['Times Visited']++;
+        res.write("<h1>Oh, I've seen you " + fingerPrintLog[userHash]['Times Visited'] + " times now</h1>")
     }
     // Fingerprint doesn't exist; user has not visited before
     else {
@@ -27,13 +27,14 @@ app.get('/', (req, res) => {
         fingerPrintLog[userHash]['Accept Language'] = acceptLanguage;
         fingerPrintLog[userHash]['Times Visited'] = 1;
         console.log('This person has not visited before!')
+        res.write("<h1>Huh, I've never seen you before. But I have now....</h1>")
     }
-    createReadStream('index.html').pipe(res);
     fs.writeFile('fingerprintlog.json', JSON.stringify(fingerPrintLog), (error) => {
         if(error) {
             console.log(error)
         }
     })
+    createReadStream('index.html').pipe(res);
 });
 
 app.listen(8080);
