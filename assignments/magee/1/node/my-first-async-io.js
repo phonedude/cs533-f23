458fbda90ea9af6f47c964fde404c89695e07038
Brian Llinas Marimon <bllin001@odu.edu>
2023-12-14 10:42:52 -0500

    Delete assignments/magee directory

--- assignments/magee/1/node/my-first-async-io.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-const contents = fs.readFile(process.argv[2], 'utf8', (err, contents) => {
-    if (err) {
-      throw err
-    }
-    const lines = contents.split('\n').length - 1;
-    console.log(lines);
-})
