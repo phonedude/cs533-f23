f16fa2772ebe0acf3a3135fc110158e18320e27e
AdamMPieroni <apier006@odu.edu>
2023-12-14 22:00:01 -0500

    added assignments 6 and 7

---
+++ assignments/pieroni/7/README.md
@@ -0,0 +1,14 @@
+## Assignment 7, CS 433/533 Web Security, Fall 2023
+
+Due: 2023-12-13
+
+Points available: 15
+
+# Adam Michael Pieroni
+
+This assignment consisted of creating and managing a webserver using Node.js.  Within the webserver you can see ATTEMPTED phishing site to capture login credentials of potential victims.  (EDUCATIONAL PURPOSES ONLY)  
+
+
+This webpage is a user created site of the 2007 myspace.  A recreation used by fans from the original MySpace website. 
+
+# Link to Youtube Video:  https://youtu.be/Uil7-hcZ_x4
---
+++ assignments/pieroni/7/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/save', (req, res) => {
+    const content = req.body.content;
+
+    // Append the content to a text file
+    fs.appendFile('output.txt', `${content}\n`, (err) => {
+        if (err) throw err;
+        console.log('Content saved to file!');
+    });
+
+    res.redirect('/');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
