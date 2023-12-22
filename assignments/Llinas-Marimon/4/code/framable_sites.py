3099a1ba0d14b39e8de16dd7b4e42ef3dabd94af
Brian Llinas Marimon <bllin001@odu.edu>
2023-12-21 19:33:54 -0500

    Delete assignments/Llinas-Marimon/4 directory

--- assignments/Llinas-Marimon/4/code/framable_sites.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('framable/report/iframe_report.csv')
-df
-
-filtered_df = df[df['Framable'] == 'Yes'].Site
-filtered_df.to_csv('framable/report/framable_sites.csv', index =False)
-print(filtered_df)
